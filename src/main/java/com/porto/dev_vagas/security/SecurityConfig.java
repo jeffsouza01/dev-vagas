0ed7620bb56b78e8d6930b1928b3a7a29fa42b9e
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2024-10-26 22:29:06 -0300

    Include Security Filter

--- src/main/java/com/porto/dev_vagas/security/SecurityConfig.java
+++ src/main/java/com/porto/dev_vagas/security/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.porto.dev_vagas.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -16,9 +23,17 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/candidate/**").permitAll()
                             .requestMatchers("/company/**").permitAll()
+                            .requestMatchers("/auth/**").permitAll()
                             .requestMatchers("/h2-console/**").permitAll();
                     auth.anyRequest().authenticated();
+
                 })
+                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
