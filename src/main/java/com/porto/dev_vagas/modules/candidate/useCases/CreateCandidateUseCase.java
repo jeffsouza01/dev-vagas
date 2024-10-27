e571b8149c3a7e139511a183c6cb6e60c76f35be
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2024-10-26 22:33:01 -0300

    Include PasswordEncoder

--- src/main/java/com/porto/dev_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/com/porto/dev_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import com.porto.dev_vagas.exceptions.UserFoundException;
 import com.porto.dev_vagas.modules.candidate.Candidate;
 import com.porto.dev_vagas.modules.candidate.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,9 @@ public class CreateCandidateUseCase {
 
     @Autowired
     private CandidateRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public Candidate execute(Candidate candidate) {
 
@@ -20,6 +24,9 @@ public class CreateCandidateUseCase {
                     throw new UserFoundException();
                 });
 
+        var pass = passwordEncoder.encode(candidate.getPassword());
+
+        candidate.setPassword(pass);
         return this.repository.save(candidate);
     }
 }
--- src/main/java/com/porto/dev_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/com/porto/dev_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -4,6 +4,8 @@ import com.porto.dev_vagas.exceptions.UserFoundException;
 import com.porto.dev_vagas.modules.company.entities.Company;
 import com.porto.dev_vagas.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +14,19 @@ public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository repository;
 
+    @Autowired
+    private PasswordEncoder encoder;
+
     public Company execute(Company company) {
 
         this.repository.findByUsernameOrEmail(company.getUsername(), company.getEmail())
                 .ifPresent(company1 -> {
                     throw new UserFoundException();
                 });
+
+        var passEncoder = encoder.encode(company.getPassword());
+
+        company.setPassword(passEncoder);
 
         return this.repository.save(company);
     }
