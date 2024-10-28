ed61b369196483a03bc3689b01116f67b89a2ce9
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2024-10-27 22:30:56 -0300

    include SwaggerDocumentation

--- src/main/java/com/porto/dev_vagas/DevVagasApplication.java
+++ src/main/java/com/porto/dev_vagas/DevVagasApplication.java
@@ -1,10 +1,23 @@
 package com.porto.dev_vagas;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Dev Vagas",
+				description = "API para gestão de vagas",
+				version = "1.0"
+		))
+@SecurityScheme(name = "jwt_auth", scheme = "bearer",
+		bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class DevVagasApplication {
 
 	public static void main(String[] args) {
