7997096311111f172252b7755b0d7bc86339075d
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2024-10-27 22:45:16 -0300

    Include PREAUTHORIZE on create JOB

--- src/main/java/com/porto/dev_vagas/modules/company/controllers/JobController.java
+++ src/main/java/com/porto/dev_vagas/modules/company/controllers/JobController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/job")
+@RequestMapping("/company/job")
 public class JobController {
 
     @Autowired
     private CreateJobUseCase jobUseCase;
 
     @PostMapping("/create")
+    @PreAuthorize("hasRole('COMPANY')")
     public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO jobDTO,
                                          HttpServletRequest request) {
 
