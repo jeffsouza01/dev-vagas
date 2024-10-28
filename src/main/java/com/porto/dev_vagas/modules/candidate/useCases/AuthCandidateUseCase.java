d098c80a9d2f6cd9c846ca51ed73c16e0ff2cad0
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2024-10-27 22:38:53 -0300

    Update JWT Creation including ROLES

--- src/main/java/com/porto/dev_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
+++ src/main/java/com/porto/dev_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -47,7 +47,7 @@ public class AuthCandidateUseCase {
         var token = JWT.create()
                 .withIssuer("Dev_Vagas")
                 .withSubject(candidate.getId().toString())
-                .withClaim("roles", List.of("candidate"))
+                .withClaim("roles", List.of("CANDIDATE"))
                 .withExpiresAt(ExpirationTimeToken())
                 .sign(algorithm);
 
--- src/main/java/com/porto/dev_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/porto/dev_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -3,6 +3,7 @@ package com.porto.dev_vagas.modules.company.useCases;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.porto.dev_vagas.modules.company.dtos.AuthCompanyDTO;
+import com.porto.dev_vagas.modules.company.dtos.AuthCompanyResponseDTO;
 import com.porto.dev_vagas.modules.company.repositories.CompanyRepository;
 import com.porto.dev_vagas.providers.JWTService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,9 @@ import org.springframework.stereotype.Service;
 import javax.naming.AuthenticationException;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
 
 @Service
 public class AuthCompanyUseCase {
@@ -30,7 +34,7 @@ public class AuthCompanyUseCase {
     @Autowired
     private JWTService jwtService;
 
-    public String execute(AuthCompanyDTO authDTO) throws AuthenticationException {
+    public AuthCompanyResponseDTO execute(AuthCompanyDTO authDTO) throws AuthenticationException {
         var company = repository.findByEmail(authDTO.email()).orElseThrow(
                 () -> new UsernameNotFoundException("Email / Password not found"));
 
@@ -43,10 +47,18 @@ public class AuthCompanyUseCase {
         //Geração Token
         Algorithm algorithm = Algorithm.HMAC256(secret);
 
-        return JWT.create().withIssuer("dev_vagas")
+        var token = JWT.create().withIssuer("dev_vagas")
                 .withExpiresAt(ExpirationTimeToken())
                 .withSubject(company.getId().toString())
+                .withClaim("roles", List.of("COMPANY"))
                 .sign(algorithm);
+
+        var authCompanyDTO = AuthCompanyResponseDTO.builder()
+                .access_token(token)
+                .expiresTokenAt(LocalDateTime.ofInstant(ExpirationTimeToken(), ZoneOffset.systemDefault()))
+                .build();
+
+        return authCompanyDTO;
     }
 
 
