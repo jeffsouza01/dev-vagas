c3dfadf4235a615fb86639c16e8b504749eb86be
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2024-10-27 22:42:11 -0300

    Update SecurityConfig For SWAGGER

--- src/main/java/com/porto/dev_vagas/security/SecurityConfig.java
+++ src/main/java/com/porto/dev_vagas/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.porto.dev_vagas.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,24 +12,39 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+
+
 
     @Autowired
     private SecurityFilter securityFilter;
+
+    @Autowired
+    private SecurityCandidateFilter securityCandidateFilter;
+
+    private static final String[] SWAGGER_LIST = {
+            "/swagger-ui/**",
+            "/v3/**",
+            "/swagger-resources/**"
+    };
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/candidate/**").permitAll()
-                            .requestMatchers("/company/**").permitAll()
+                    auth.requestMatchers("/candidate/").permitAll()
+                            .requestMatchers("/company/").permitAll()
                             .requestMatchers("/auth/**").permitAll()
+                            .requestMatchers(SWAGGER_LIST).permitAll()
                             .requestMatchers("/h2-console/**").permitAll();
+
                     auth.anyRequest().authenticated();
 
                 })
                 .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
+                .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
                 .build();
     }
 
